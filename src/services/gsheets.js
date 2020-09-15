edb239c1a2d8b9d593cfcba5c55b6d03979fd8f0
Arun R <yet.another.movie@gmail.com>
2020-09-15 16:51:44 +0530

    Add: Service to get google sheet

---
+++ src/services/gsheets.js
@@ -0,0 +1,17 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+export const WP = {
+  doc: null,
+
+  init() {
+    this.doc = new GoogleSpreadsheet(process.env.VUE_APP_SHEET_ID);
+
+    // OR use API key -- only for read-only access to public sheets
+    this.doc.useApiKey(process.env.VUE_APP_API_KEY);
+  },
+
+  async getSheetRows() {
+    await this.doc.loadInfo(); // loads document properties and worksheets
+    return await this.doc.sheetsByIndex[0].getRows(); // or use doc.sheetsById[id]
+  }
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
       </WorldMapVue>
     </div>
     <modal name="countryinfo">
-      <h2>Country {{ activeCountry }} has been selected</h2>
+      <h2>Country {{ clickedCountry }} has been selected</h2>
     </modal>
   </div>
 </template>
@@ -24,6 +24,7 @@
 // @ is an alias to /src
 import WorldMapVue from "world-map-vue";
 import Vue from "vue";
+import { WP } from "@/services/gsheets";
 
 export default {
   name: "Home",
@@ -42,6 +43,11 @@ export default {
     };
   },
 
+  async mounted() {
+    WP.init();
+    console.log(await WP.getSheetRows());
+  },
+
   methods: {
     hoverStart(code) {
       this.hoverSavedColor = this.countries[code];
@@ -50,14 +56,13 @@ export default {
       // Update your data/property to be displayed on the overlay.
     },
 
-    // hoverEnd(code) {
-    // Vue.set(this.countries, code, this.savedColor);
-    //},
+    hoverEnd() {},
 
     selectCountry(code) {
       Vue.set(this.countries, code, "green");
       this.clickedCountry = code;
       this.$modal.show("countryinfo");
+      console.log(document.getElementById(code).getAttribute("title"));
     }
   }
 };
